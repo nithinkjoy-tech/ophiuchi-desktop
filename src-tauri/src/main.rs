75a6b3cd2fdcdfd96b7f9c5c7b678c4c9b034af0
cheeselemon <cheeselemon.yarn@gmail.com>
2024-12-11 11:00:59 +0900

    fix: fs scope for resources, added shell plugin for docker control

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -259,6 +259,7 @@ fn main() {
     .plugin(tauri_plugin_opener::init())
     .plugin(tauri_plugin_fs::init())
     .plugin(tauri_plugin_dialog::init())
+    .plugin(tauri_plugin_shell::init())
     .invoke_handler(tauri::generate_handler![
       check_docker_installed,
       add_cert_to_keychain,
