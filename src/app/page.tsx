d9e00492637b38864eb7b0804ddd323434ab9509
cheeselemon <cheeselemon.yarn@gmail.com>
2024-12-11 20:25:42 +0900

    chore: cleanup code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,7 @@ import ProxyListComponent from "@/components/page-components/proxy-list";
 import dynamic from "next/dynamic";
 
 function Home() {
-  return (
-    <div className="">
-      <ProxyListComponent />
-    </div>
-  );
+  return <ProxyListComponent />;
 }
 
 export default dynamic(() => Promise.resolve(Home), {
--- src/components/page-components/proxy-list/index.tsx
+++ src/components/page-components/proxy-list/index.tsx
@@ -1,10 +1,7 @@
 "use client";
 
-import proxyListStore from "@/stores/proxy-list";
 import ProxyListTable from "./table";
 
 export default function ProxyListComponent() {
-  const { proxyList } = proxyListStore();
-
   return <ProxyListTable />;
 }
